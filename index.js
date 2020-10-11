290ba1bdb4da2cf58728d113f44ad1a1d0ed6b86
aqib <aqib.javed@concurrent.systems>
2020-10-12 01:30:08 +0500

    major changes

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
+const packageJSON = require('./package.json')
+
+console.log(`
+name: ${packageJSON.name}
+description: ${packageJSON.description}
+version: ${packageJSON.version}
+`)
+
 console.log(`
 test npm asdfs
 
